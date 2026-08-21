5896fe923029a897a0cf436ea8b5584c3bdf6551
ale_torelli <torelli.alessandro@gmail.com>
2026-08-21 11:38:14 +0200

    feat: implemented websocket handshake route

--- ScalaParty/infrastructure/src/main/scala/com/unibo/scalaparty/infrastructure/adapters/ServerApp.scala
+++ ScalaParty/infrastructure/src/main/scala/com/unibo/scalaparty/infrastructure/adapters/ServerApp.scala
@@ -6,6 +6,9 @@ import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.Router
+import org.http4s.server.websocket.WebSocketBuilder2
+import org.http4s.websocket.WebSocketFrame
+import fs2.{Pipe, Stream}
 
 object ServerApp extends IOApp.Simple {
 
@@ -14,17 +17,29 @@ object ServerApp extends IOApp.Simple {
       Ok("Scala Party Server is up and running!")
   }
 
+  def wsRoute(wsb: WebSocketBuilder2[IO]): HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root / "ws" =>
+      val receive: Pipe[IO, WebSocketFrame, Unit] =
+        _.evalMap(frame => IO.println(s"Ricevuto dal client: $frame"))
+
+      val send: Stream[IO, WebSocketFrame] = Stream.never[IO]
+
+      wsb.build(send, receive)
+  }
+  
   // Router che mappa la rotta sulla root "/"
-  val httpApp = Router(
-    "/" -> baseRoute
+  def httpApp(wsb: WebSocketBuilder2[IO]) = Router(
+    "/" -> baseRoute,
+    "/" -> wsRoute(wsb)
   ).orNotFound
 
+  // Dopo 60s senza scambio di messaggi la connessione si chiude automaticamente
   val run: IO[Unit] =
     EmberServerBuilder
       .default[IO]
       .withHost(ipv4"0.0.0.0")
       .withPort(port"8081")
-      .withHttpApp(httpApp)
+      .withHttpWebSocketApp(wsb => httpApp(wsb))
       .build
-      .use(_ => IO.never) // Mantiene il server in esecuzione indefinitamente
+      .use(_ => IO.never)
 }
