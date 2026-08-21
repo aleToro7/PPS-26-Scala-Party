5871e513871eec747171548ffb9dff7fc522e194
Diottanax <fd.diotallevi@gmail.com>
2026-08-21 22:12:05 +0200

    refactor: no need to pass the world to the GameEngine for updates

--- ScalaParty/core/src/main/scala/com/unibo/scalaparty/core/engine/GameEngine.scala
+++ ScalaParty/core/src/main/scala/com/unibo/scalaparty/core/engine/GameEngine.scala
@@ -6,9 +6,8 @@ import com.unibo.scalaparty.core.dto.{Entity, PlayerCommand}
 trait GameEngine:
   /** Updates the state of the game world based on the provided player commands and the elapsed time.
    *
-   * @param world the current state of the game world, represented as a list of entities
    * @param list  a list of player commands to be processed
    * @param dt    the elapsed time since the last update, in milliseconds
    * @return a new list of entities representing the updated state of the game world
    */
-  def update(world: List[Entity], list: List[PlayerCommand], dt: Long): List[Entity]
+  def update(list: List[PlayerCommand], dt: Long): List[Entity]
