7bc3d42d6c03fc62532d795324c81c1c8b0c7e41
Diottanax <fd.diotallevi@gmail.com>
2026-08-22 19:00:50 +0200

    feat: add entityId to spaceship's dto

--- ScalaParty/core/src/main/scala/com/unibo/scalaparty/core/dto/EntityDto.scala
+++ ScalaParty/core/src/main/scala/com/unibo/scalaparty/core/dto/EntityDto.scala
@@ -1,5 +1,6 @@
 package com.unibo.scalaparty.core.dto
 
+import com.unibo.scalaparty.core.ecs.EntityId
 import com.unibo.scalaparty.core.geometry.{Point2D, Vector2D}
 
 
@@ -8,8 +9,9 @@ import com.unibo.scalaparty.core.geometry.{Point2D, Vector2D}
 enum EntityDto:
 
   /** Represents a player spaceship.
-   *
+   * 
+   * @param id the unique identifier of the spaceship entity
    * @param position the current 2D spatial coordinates of the spaceship
    * @param velocity the current movement vector representing the speed and direction of the spaceship
    */
-  case Spaceship(position: Point2D, velocity: Vector2D)
+  case Spaceship(id: EntityId, position: Point2D, velocity: Vector2D)
