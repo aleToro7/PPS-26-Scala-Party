9bc7bd9e0ab91c7d8976cba388d299aa9b21affb
Diottanax <fd.diotallevi@gmail.com>
2026-08-23 16:09:25 +0200

    test: spaceships should have the appropriate entity type component

--- ScalaParty/core/src/test/scala/com/unibo/scalaparty/core/ecs/EntityFactorySpec.scala
+++ ScalaParty/core/src/test/scala/com/unibo/scalaparty/core/ecs/EntityFactorySpec.scala
@@ -2,14 +2,17 @@ package com.unibo.scalaparty.core.ecs
 
 import com.unibo.scalaparty.core.geometry.{Point2D, Vector2D}
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.must.Matchers
-import org.scalatest.matchers.should.Matchers.should
-import org.scalatest.matchers.should.Matchers.contain
+import org.scalatest.matchers.should.Matchers.{contain, should}
 
 class EntityFactorySpec extends AnyFlatSpec:
 
   "EntityFactory" should "create a spaceship entity with the correct components" in:
     val position = Point2D(10, 20)
     val velocity = Vector2D(1, 1)
-    val (entityId, components) = EntityFactory.createSpaceship(position, velocity)
-    components should contain allOf(PositionComponent(position), MovementComponent(velocity))
+    val (entityId, components) =
+      EntityFactory.createSpaceship(position, velocity)
+    components should contain allOf (
+      PositionComponent(position),
+      MovementComponent(velocity),
+      EntityTypeComponent(EntityType.Spaceship)
+    )
