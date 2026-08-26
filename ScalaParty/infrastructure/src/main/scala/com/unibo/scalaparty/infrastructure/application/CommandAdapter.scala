4cb75a9d235245b63e00db8660841d9ad3c63e05
aleToro7 <toryst4rk@gmail.com>
2026-08-26 17:51:55 +0200

    feat: add CommandAdapter to translate network intents to ECS DTOs

---
+++ ScalaParty/infrastructure/src/main/scala/com/unibo/scalaparty/infrastructure/application/CommandAdapter.scala
@@ -0,0 +1,16 @@
+package com.unibo.scalaparty.infrastructure.application
+
+import com.unibo.scalaparty.core.ecs.EntityId
+import com.unibo.scalaparty.core.model.PlayerCommand as IntentCommand
+import com.unibo.scalaparty.core.dto.PlayerCommand as DtoCommand
+
+object CommandAdapter:
+
+  extension (intent: IntentCommand)
+
+    /** Converts the raw network intent into the application domain DTO for the ECS.
+     *  Returns None if the command is not yet supported by the DTO.
+     */
+    def toDto(entityId: EntityId): Option[DtoCommand] = intent match
+      case IntentCommand.Rotate(angle) => Some(DtoCommand.Rotate(entityId, angle))
+      case IntentCommand.Shoot         => None
---
+++ ScalaParty/infrastructure/src/test/scala/com/unibo/scalaparty/infrastructure/application/CommandAdapterSpec.scala
@@ -0,0 +1,29 @@
+package com.unibo.scalaparty.infrastructure.application
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import com.unibo.scalaparty.core.ecs.EntityId
+import com.unibo.scalaparty.core.model.PlayerCommand as IntentCommand
+import com.unibo.scalaparty.core.dto.PlayerCommand as DtoCommand
+
+import CommandAdapter.*
+
+class CommandAdapterSpec extends AnyWordSpec with Matchers:
+
+  "CommandAdapter".should:
+
+    "translate a Rotate intent into a Rotate DTO with the provided EntityId".in:
+      val entityId = EntityId.generate()
+      val intent   = IntentCommand.Rotate(45.0)
+
+      val result = intent.toDto(entityId)
+
+      result shouldBe Some(DtoCommand.Rotate(entityId, 45.0))
+
+    "return None for unsupported intents like Shoot".in:
+      val entityId = EntityId.generate()
+      val intent   = IntentCommand.Shoot
+
+      val result = intent.toDto(entityId)
+
+      result shouldBe None
