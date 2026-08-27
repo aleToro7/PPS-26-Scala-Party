8d898b258ab41b317564d8ad212c922d3b63233e
aleToro7 <toryst4rk@gmail.com>
2026-08-27 15:49:50 +0200

    feat: implement Circe codecs for network DTOs

--- ScalaParty/infrastructure/src/main/scala/com/unibo/scalaparty/infrastructure/network/dto/ProtocolCodecs.scala
+++ ScalaParty/infrastructure/src/main/scala/com/unibo/scalaparty/infrastructure/network/dto/ProtocolCodecs.scala
@@ -1,8 +1,20 @@
 package com.unibo.scalaparty.infrastructure.network.dto
 
-import io.circe.Decoder
+import io.circe.*
 import io.circe.generic.semiauto.*
-import com.unibo.scalaparty.core.model.PlayerCommand
+import com.unibo.scalaparty.core.dto.{EntityDto, PlayerCommand}
+import com.unibo.scalaparty.core.model.{GameEvent, MatchState}
+import com.unibo.scalaparty.core.ecs.EntityId
 
 object ProtocolCodecs:
+  given Decoder[EntityId] = Decoder.decodeLong.map(EntityId.fromLong)
+  given Encoder[EntityId] = Encoder.encodeLong.contramap(_.value)
+  
+  // Inbound (Client -> Server)
   given Decoder[PlayerCommand] = deriveDecoder
+
+  // Outbound (Server -> Client)
+  given Encoder[PlayerCommand] = deriveEncoder
+  given Encoder[EntityDto] = deriveEncoder
+  given Encoder[MatchState] = deriveEncoder
+  given Encoder[GameEvent] = deriveEncoder
