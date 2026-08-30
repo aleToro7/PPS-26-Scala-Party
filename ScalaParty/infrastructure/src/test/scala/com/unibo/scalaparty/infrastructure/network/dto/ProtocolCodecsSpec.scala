6e951f6e5e525353fef6e70f0579f83ee13c7e24
aleToro7 <toryst4rk@gmail.com>
2026-08-30 15:08:47 +0200

    refactor: ProtocolCodecsSpec

--- ScalaParty/infrastructure/src/test/scala/com/unibo/scalaparty/infrastructure/network/dto/ProtocolCodecsSpec.scala
+++ ScalaParty/infrastructure/src/test/scala/com/unibo/scalaparty/infrastructure/network/dto/ProtocolCodecsSpec.scala
@@ -11,15 +11,15 @@ import com.unibo.scalaparty.infrastructure.network.dto.ProtocolCodecs.given
 
 class ProtocolCodecsSpec extends AnyWordSpec with Matchers:
 
-  "ProtocolCodecs Encoders" should {
+  "ProtocolCodecs Encoders" should:
 
-    "correctly serialize an EntityId into a JSON number" in {
+    "correctly serialize an EntityId into a JSON number" in:
       val entityId = EntityId.fromLong(42L)
       val json = entityId.asJson.noSpaces
-      json shouldEqual "42"
-    }
 
-    "correctly serialize a MatchState containing DTOs" in {
+      json shouldEqual "42"
+
+    "correctly serialize a MatchState containing DTOs" in:
       val entityId = EntityId.fromLong(1L)
       val spaceship = EntityDto.Spaceship(entityId, Point2D(10.0, 20.0), Vector2D(1.0, 0.0))
       val state = MatchState(tick = 100L, entities = List(spaceship))
@@ -29,5 +29,3 @@ class ProtocolCodecsSpec extends AnyWordSpec with Matchers:
       json should include(""""tick":100""")
       json should include(""""id":1""")
       json should include(""""x":10.0""")
-    }
-  }
