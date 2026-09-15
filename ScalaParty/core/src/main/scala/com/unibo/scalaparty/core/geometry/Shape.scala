39592f1a1238fb08df77cb1feb9468582cc3b77e
Diottanax <fd.diotallevi@gmail.com>
2026-09-16 00:28:34 +0200

    feat: add intersection logic for circles

--- ScalaParty/core/src/main/scala/com/unibo/scalaparty/core/geometry/Shape.scala
+++ ScalaParty/core/src/main/scala/com/unibo/scalaparty/core/geometry/Shape.scala
@@ -1,15 +1,14 @@
 package com.unibo.scalaparty.core.geometry
 
+import com.unibo.scalaparty.core.geometry.Shape.{Circle, Rectangle}
+
 /** Represents a geometric shape in a two-dimensional space.
  *  This sealed trait defines the different types of shapes that can be represented, including circles, rectangles, squares, and polygons.
  */
 enum Shape:
   case Circle(radius: Double, center: Point2D)
   case Rectangle(width: Double, height: Double, center: Point2D)
-  case Square(side: Double, center: Point2D)
   case Triangle(a: Point2D, b: Point2D, c: Point2D)
-  
-private val tolerance = 1e-6
 
 extension [A <: Shape](self: A)
 
@@ -19,21 +18,18 @@ extension [A <: Shape](self: A)
    *  @return true if the shapes intersect, false otherwise
    */
   infix def intersects[B <: Shape](other: B): Boolean = (self, other) match
-    case (Shape.Rectangle(width1, height1, center1), Shape.Rectangle(width2, height2, center2)) =>
-      val (rect1Left, rect1Right, rect1Bottom, rect1Top) = getCorners(Shape.Rectangle(width1, height1, center1))
-      val (rect2Left, rect2Right, rect2Bottom, rect2Top) = getCorners(Shape.Rectangle(width2, height2, center2))
-
+    case (r1: Rectangle, r2: Rectangle) =>
+      val (rect1Left, rect1Right, rect1Bottom, rect1Top) = getCorners(r1)
+      val (rect2Left, rect2Right, rect2Bottom, rect2Top) = getCorners(r2)
       val overlapX = math.max(rect1Left, rect2Left) <= math.min(rect1Right, rect2Right)
       val overlapY = math.max(rect1Bottom, rect2Bottom) <= math.min(rect1Top, rect2Top)
       overlapX && overlapY
+    case (c1: Circle, c2: Circle) =>
+      val distance = (c1.center - c2.center).module
+      distance <= c1.radius + c2.radius
     case _ => false
-    
 
-/** Returns the four corners of a rectangle.
- *  @param r the rectangle
- *  @return a tuple containing the left, right, bottom, and top coordinates
- */
-private def getCorners(r: Shape.Rectangle): (Double, Double, Double, Double) =
+private def getCorners(r: Rectangle): (Double, Double, Double, Double) =
   val left = r.center.x - r.width / 2.0
   val right = r.center.x + r.width / 2.0
   val bottom = r.center.y - r.height / 2.0
--- ScalaParty/core/src/test/scala/com/unibo/scalaparty/core/geometry/IntersectionSpec.scala
+++ ScalaParty/core/src/test/scala/com/unibo/scalaparty/core/geometry/IntersectionSpec.scala
@@ -4,25 +4,38 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.shouldBe
 
 class IntersectionSpec extends AnyFlatSpec:
-  
-  
 
-  "A Rectangle" should "intersect when they overlap" in :
+  "A Rectangle" should "intersect when they overlap" in:
     val a = Shape.Rectangle(4.0, 2.0, Point2D(0.0, 0.0))
     val b = Shape.Rectangle(2.0, 2.0, Point2D(1.0, 0.0))
     (a intersects b) shouldBe true
 
-  it should "intersect when they touch only at the border" in :
+  it should "intersect when they touch only at the border" in:
     val a = Shape.Rectangle(4.0, 2.0, Point2D(0.0, 0.0))
     val b = Shape.Rectangle(2.0, 2.0, Point2D(3.0, 0.0))
     (a intersects b) shouldBe true
 
-  it should "intersect when one rectangle contains the other" in :
+  it should "intersect when one rectangle contains the other" in:
     val outerRect = Shape.Rectangle(10.0, 10.0, Point2D(0.0, 0.0))
     val innerRect = Shape.Rectangle(2.0, 2.0, Point2D(1.0, 1.0))
     (outerRect intersects innerRect) shouldBe true
 
-  it should "not intersect when they are separated by a gap" in :
+  it should "not intersect when they are separated by a gap" in:
     val a = Shape.Rectangle(4.0, 2.0, Point2D(0.0, 0.0))
     val b = Shape.Rectangle(2.0, 2.0, Point2D(3.01, 0.0))
+    (a intersects b) shouldBe false
+
+  "A Circle" should "intersect when they overlap" in:
+    val a = Shape.Circle(2.0, Point2D(0.0, 0.0))
+    val b = Shape.Circle(2.0, Point2D(1.0, 1.0))
+    (a intersects b) shouldBe true
+
+  it should "intersect when they touch only at the border" in:
+    val a = Shape.Circle(2.0, Point2D(0.0, 0.0))
+    val b = Shape.Circle(2.0, Point2D(4.0, 0.0))
+    (a intersects b) shouldBe true
+
+  it should "not intersect when they are separated by a gap" in:
+    val a = Shape.Circle(2.0, Point2D(0.0, 0.0))
+    val b = Shape.Circle(2.0, Point2D(5.0, 0.0))
     (a intersects b) shouldBe false
