9d080048f587d827911b6fcd86ee37545625ed0b
aleToro7 <toryst4rk@gmail.com>
2026-09-15 18:00:18 +0200

    refactor: update GameEngineSpec to use GameSettings

--- ScalaParty/core/src/test/scala/com/unibo/scalaparty/core/engine/GameEngineSpec.scala
+++ ScalaParty/core/src/test/scala/com/unibo/scalaparty/core/engine/GameEngineSpec.scala
@@ -4,20 +4,24 @@ import com.unibo.scalaparty.core.ecs.{EntityId, GameWorld}
 import com.unibo.scalaparty.core.ecs.systems.{System, SystemPipeline}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import com.unibo.scalaparty.core.model.GameSettings
 
 class GameEngineSpec extends AnyFlatSpec with Matchers:
 
   private val emptyPipeline = SystemPipeline()
+  private val testSettings = GameSettings(
+    worldWidth = 800,
+    worldHeight = 600,
+    spaceshipSpeed = 5.0,
+    spaceshipRotationSpeed = 0.5
+  )
 
   "A GameEngine" should "not create a new world if the pipeline is empty" in:
     val player = EntityId.generate()
     val someTime = 100L // 100 milliseconds
     val engine = GameEngine(GameConfig(
       players = List(player),
-      worldWidth = 800,
-      worldHeight = 600,
-      spaceshipSpeed = 5.0,
-      spaceshipRotationSpeed = 0.5,
+      settings = testSettings,
       pipeline = emptyPipeline
     ))
     val initialState = engine.update(Nil, 0)
@@ -30,10 +34,7 @@ class GameEngineSpec extends AnyFlatSpec with Matchers:
     val someTime = 100L // 100 milliseconds
     val engine = GameEngine(GameConfig(
       players = List(player),
-      worldWidth = 800,
-      worldHeight = 600,
-      spaceshipSpeed = 5.0,
-      spaceshipRotationSpeed = 0.5,
+      settings = testSettings,
       pipeline = SystemPipeline(clearWorldSystem)
     ))
     val initialState = engine.update(Nil, 0)
