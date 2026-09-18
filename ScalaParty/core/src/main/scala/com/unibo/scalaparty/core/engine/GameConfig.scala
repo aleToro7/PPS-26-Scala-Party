11c7a7dbeff2b03622d56a7920565325d568574c
aleToro7 <toryst4rk@gmail.com>
2026-09-18 11:49:52 +0200

    fix: add import ShootingSystem for merge

--- ScalaParty/core/src/main/scala/com/unibo/scalaparty/core/engine/GameConfig.scala
+++ ScalaParty/core/src/main/scala/com/unibo/scalaparty/core/engine/GameConfig.scala
@@ -1,7 +1,7 @@
 package com.unibo.scalaparty.core.engine
 
 import com.unibo.scalaparty.core.ecs.EntityId
-import com.unibo.scalaparty.core.ecs.systems.{MovementSystem, SystemPipeline}
+import com.unibo.scalaparty.core.ecs.systems.{MovementSystem, ShootingSystem, SystemPipeline}
 import com.unibo.scalaparty.core.model.GameSettings
 
 /** Configuration for the game engine.
