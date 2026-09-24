082545149fb9f833f3175d70a4317fa70305f507
aleToro7 <toryst4rk@gmail.com>
2026-09-24 22:02:22 +0200

    test: verify player spawn and map capacity in game engine

--- ScalaParty/core/src/test/scala/com/unibo/scalaparty/core/engine/SinglePlayerGameEngineSpec.scala
+++ ScalaParty/core/src/test/scala/com/unibo/scalaparty/core/engine/SinglePlayerGameEngineSpec.scala
@@ -1,18 +1,48 @@
 package com.unibo.scalaparty.core.engine
 
+import com.unibo.scalaparty.core.dto.EntityDto
 import com.unibo.scalaparty.core.ecs.EntityId
 import com.unibo.scalaparty.core.ecs.systems.SystemPipeline
+import com.unibo.scalaparty.core.geometry.Point2D
+import com.unibo.scalaparty.core.map.{GameMap, SpawnPoint}
+import com.unibo.scalaparty.core.model.{ArenaSettings, GameSettings}
 
 val emptyPipeline = SystemPipeline()
 
 class SinglePlayerGameEngineSpec extends GameEngineSpec:
+
+  private val tolerance = 1e-9
+
+  private def spawnedSpaceships(config: GameConfig): Map[EntityId, EntityDto.Spaceship] =
+    GameEngine(config)
+      .update(Nil, 0L)
+      .collect { case spaceship: EntityDto.Spaceship => spaceship.id -> spaceship }
+      .toMap
 
   "A GameEngine with a single player" should "use a SinglePlayerGameEngine" in:
     val player = EntityId.generate()
     val engine = GameEngine(GameConfig.singlePlayer(player, pipeline = emptyPipeline))
     engine shouldBe a[SinglePlayerGameEngine]
 
-  "A SinglePlayerGameEngine" should "place the player in the center of the world" in:
+  "A SinglePlayerGameEngine" should "spawn only the player at the first spawn point of the map" in:
     val player = EntityId.generate()
-    val engine = GameEngine(GameConfig.singlePlayer(player, pipeline = emptyPipeline))
-    val state = engine.update(Nil, 0)
+    val config = GameConfig.singlePlayer(player, pipeline = emptyPipeline)
+
+    val spaceships = spawnedSpaceships(config)
+
+    spaceships.keySet shouldBe Set(player)
+    spaceships(player).position shouldBe config.map.playerSpawns.head.position
+
+  it should "move the spaceship along its spawn heading at the configured speed" in:
+    val player = EntityId.generate()
+    val map = GameMap(ArenaSettings(), List(SpawnPoint(Point2D(100, 100), heading = 90.0)))
+    val config = GameConfig(GameSettings.default, List(player), map, emptyPipeline)
+
+    val velocity = spawnedSpaceships(config)(player).velocity
+
+    velocity.x shouldBe 0.0 +- tolerance
+    velocity.y shouldBe config.settings.spaceship.speed +- tolerance
+
+  "A GameConfig" should "reject more players than the map can host" in:
+    val players = List.fill(GameMap.default.capacity + 1)(EntityId.generate())
+    an[IllegalArgumentException] should be thrownBy GameConfig(GameSettings.default, players)
